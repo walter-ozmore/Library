54ea021c43c7bf48f95256b958cbc861c5c4d7a5
walter.ozmore <walter.ozmore@BISD50114.bisd.local>
2021-04-22 16:02:21 -0500

    4/22/2021 4:02

--- Library/src/Misc/ServerMisc.java
+++ Library/src/Misc/ServerMisc.java
@@ -1,5 +1,8 @@
 package Misc;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -23,16 +26,75 @@ public class ServerMisc {
 		
 		return str;
 	}
-	public static void sendString(OutputStream out, String str) {
-		try {
+	public static void sendString(OutputStream out, String str) throws IOException {
+		
 			byte[] aName = new byte[str.length()];
 			for(int z=0;z<str.length();z++) 
 				aName[z] = (byte) str.charAt(z);
 			out.write( BaseConverter.longToByteArray( aName.length ) );
 			out.write( aName );
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+	//Files
+	public static void sendFile(OutputStream out, File file, String str) throws IOException {
+		//*
+		InputStream in = new FileInputStream( file );
+		String name = str + file.getName();
+		byte[] aName = new byte[name.length()];
+		for(int z=0;z<name.length();z++) aName[z] = (byte) name.charAt(z);
+		
+		
+		//Send filename / path length
+		out.write( BaseConverter.longToByteArray( aName.length ) );
+		//Send filename / path
+		out.write( aName );
+		//*/
+		//Send file length
+		out.write( BaseConverter.longToByteArray(file.length()) );
+			
+		copy(new FileInputStream( file ),out);
+		in.close();
+	}
+	static void copy(InputStream in, OutputStream out) throws IOException {
+		byte[] buf = new byte[8192];
+		int len = 0;
+		while ((len = in.read(buf)) != -1) {
+			out.write(buf, 0, len);
+		}
+	}
+	public static void getFile(InputStream in) throws IOException {
+		byte[] temp = new byte[8];
+		
+		//getName Length
+		temp = new byte[8];
+		in.read(temp);
+		int nameLength = (int) BaseConverter.byteArrayToLong(temp);
+		
+		//getName
+		byte[] aName = new byte[ nameLength ];
+		in.read( aName );
+		String name = "";
+		for(int z=0;z<aName.length;z++) name += (char)aName[z];
+		
+		//getFile Length
+		temp = new byte[8];
+		in.read(temp);
+		long fileLength = BaseConverter.byteArrayToLong(temp);
+		
+		new File(name).getParentFile().mkdirs();
+		copy(in, new FileOutputStream(name), fileLength );
+	}
+	static void copy(InputStream in, OutputStream out, long size) throws IOException {
+		int bufSize = 8192;
+		byte[] buf = new byte[bufSize];
+		int len = 0;
+		while ((len = in.read(buf)) != -1) {
+			out.write(buf, 0, len);
+			size -= len;
+			if(len < size) {
+				bufSize = (int)size;
+				buf = new byte[bufSize];
+			}
+			if(size == 0) break;
 		}
 	}
 	
