c1f2c42caa117f3eb5701a53fe9c7a61ccb64049
Walter Ozmore <soulsilver27@hotmail.com>
2021-07-28 19:40:00 -0500

    7/28/2021

---
+++ Library/src/Misc/SDL.java
@@ -0,0 +1,7 @@
+package Misc;
+
+public class SDL {
+	public static void print(String str) { System.out.print(str); }
+	public static void println() { System.out.println(); }
+	public static void println(String str) { System.out.println(str); }
+}
