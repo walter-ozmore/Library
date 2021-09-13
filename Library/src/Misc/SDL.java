599ddf5ede1b224f8510bada75a6d49e9d9eaef7
walter.ozmore <walter.ozmore@BISD60455.bisd.local>
2021-09-13 08:39:14 -0500

    9/13/2021

--- Library/src/Misc/SDL.java
+++ Library/src/Misc/SDL.java
@@ -1,7 +1,8 @@
 package Misc;
 
 public class SDL {
-	public static void print(String str) { System.out.print(str); }
+	public static <T> void print(T str) { System.out.print(str+""); }
 	public static void println() { System.out.println(); }
-	public static void println(String str) { System.out.println(str); }
+	public static <T> void println(T str) { System.out.println(str+""); }
+	public static void sleep(long time) { try { Thread.sleep(time); } catch (InterruptedException e) { e.printStackTrace(); } }
 }
